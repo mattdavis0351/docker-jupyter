5ac2817e0b11c94fdf872494f48ef436471aa79b
mattdavis0351 <mattdavis0351@github.com>
2020-04-23 16:31:16 -0700

    inital push

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,6 +2,6 @@
 c = get_config()
 c.NotebookApp.password = 'sha1:6f9cf44b5eec3c0e4b645a6fb4044ebf00c86842'
 c.Spawner.notebook_dir = '~/'
-c.NotebookApp.certfile = '/etc/letsencrypt/live/notebook.easiertechlabs.com/fullchain.pem'
-c.NotebookApp.keyfile = '/etc/letsencrypt/live/notebook.easiertechlabs.com/privkey.pem'
+c.NotebookApp.certfile = '/root/fullchain.pem'
+c.NotebookApp.keyfile = '/root/privkey.pem'
 c.NotebookApp.ip = '0.0.0.0'
